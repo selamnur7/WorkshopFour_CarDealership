1be8a739a271fd068f64c6f5b2bfdafa0883aab1
Selam Nur <selamnur7@gmail.com>
2024-05-12 14:43:34 -0400

    Second Phase

--- src/main/java/com/ps/DealershipFileManager.java
+++ src/main/java/com/ps/DealershipFileManager.java
@@ -1,5 +1,44 @@
 package com.ps;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
 
+    private void getDealership(){
+        try{
+            BufferedReader bufReader = new BufferedReader(new FileReader("DealershipInventory"));
+            int lineCounter = 0;
+            String input;
+            while ((input = bufReader.readLine()) != null) {
+                if (lineCounter == 0) {
+                    String[] dealershipSplitLine = input.split("\\|");
+                    String name = dealershipSplitLine[0];
+                    String address = dealershipSplitLine[1];
+                    String phone = dealershipSplitLine[2];
+
+                    lineCounter++;
+
+                } else {
+                    String[] vehicleSplitLine = input.split("\\|");
+                    int vin = Integer.parseInt(vehicleSplitLine[0]);
+                    int year = Integer.parseInt(vehicleSplitLine[1]);
+                    String make = vehicleSplitLine[2];
+                    String model = vehicleSplitLine[3];
+                    String vehicleType = vehicleSplitLine[4];
+                    String color = vehicleSplitLine[5];
+                    int odometer = Integer.parseInt(vehicleSplitLine[6]);
+                    double price = Double.parseDouble(vehicleSplitLine[7]);
+                    Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+                    lineCounter++;
+
+                }
+            }
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
